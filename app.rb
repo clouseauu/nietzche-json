2fb1bf1f6780aee148260f8aa840e3ecf2df66f1
Daniel Angel-Bradford <locusdelicti@gmail.com>
2014-11-23 13:45:10 +1100

    return json content type :rage1:

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'json'
 require 'pry'
 
 class Quote < ActiveRecord::Base; end
 
 
 get '/?:number?' do
+  content_type :json
   number = Integer(params[:number] || 1) rescue 1
+  number = 10 if number > 10
   @quotes = Quote.limit( number ).order( "RANDOM()" )
   @quotes.to_json
 end
